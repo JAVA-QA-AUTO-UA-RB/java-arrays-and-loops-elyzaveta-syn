33500138d943fc00884ef53dc0d3b552be87cb08
elyzaveta-syn <vam.fox86@gmail.com>
2025-08-05 21:29:38 +0200

    wrote three parts of the quest

--- src/CastleAdventure.java
+++ src/CastleAdventure.java
@@ -5,27 +5,68 @@ public class CastleAdventure {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Нижче перша частина завдання для прикладу
-        // Там де в завданні вказані початкові дані у вигляді масиву - ініціалізуйте окремий масив
-        // Нижче обробляйте масив, додайте логіку обробки
         // Частина 1: Увійди в Замок
         int[] secretCode = {7, 14, 28, 56, 112};
-        System.out.println("Введіть 5 чисел, щоб відкрити двері замку:");
-        boolean isCodeCorrect = true;
+        System.out.println("Введи 5 чисел, щоб відкрити двері замку:");
+        boolean correctCode = true;
 
         for (int i = 0; i < secretCode.length; i++) {
-            int userInput = scanner.nextInt(); // питаємо в користувача чергове число
-            if (userInput != secretCode[i]) { // перевіряємо число проти i-го елемента масиву з правильним кодом
-                isCodeCorrect = false; // якщо натрапляємо на неправильно введене число - усю комбінацію вважаємо невірною
+            int userInput = scanner.nextInt();
+            if (userInput != secretCode[i]) {
+                correctCode = false;
             }
         }
 
-        if (isCodeCorrect) {
-            System.out.println("Замок відкрито! Заходьте всередину.");
+        if (correctCode) {
+            System.out.println("Замок відкрито! Заходь всередину.");
         } else {
-            System.out.println("Невірний код. Спробуйте ще.");
+            System.out.println("Невірний код. Спробуй ще.");
         }
 
-        // Інші частини пригоди додайте тут!
+        // Частина 2: Розв'яжи магічну загадку
+        System.out.println("Блукаючи подвір'ям замку ти натрапляєш на дошку зі значеннями: 1, 12, 3, 14, 7, 10, 21, 100.");
+        System.out.println("Здійсни математичні дії та відкрий двері до внутрішньої частини замку!");
+        int[] mysteryPuzzle = {1, 12, 3, 14, 7, 10, 21, 100};
+        int sum = 0;
+        boolean correctAnswer = true;
+
+        for (int i = 0; i < mysteryPuzzle.length; i++) {
+            sum += mysteryPuzzle[i];
+        }
+
+        int userInput = scanner.nextInt();
+        if (userInput != sum) {
+            correctAnswer = false;
+        }
+
+        if (correctAnswer) {
+            System.out.println("Двері відчиняються! Скоріш біжи у наступну кімнату!");
+        } else {
+            System.out.println("Неправильно. Перерахуй.");
+        }
+
+        // Частина 3: Перемогти Залізного Дракона
+        System.out.println("Уся кімната була заповнена інструментами, механізмами та дивними інструкціями. Випадково, ти натискаєш на кнопку...");
+        System.out.println("Двері сусідньої кімнати відчиняються і перед тобою постає механічний монстр!");
+        System.out.println("Скоріше введи код деактивації!");
+        int[] codeOfDeactivation = {4, 6, 10, 14, 20};
+        boolean deactivation = true;
+
+        for (int i = 0; i < codeOfDeactivation.length; i++) {
+           userInput = scanner.nextInt();
+            if (userInput != codeOfDeactivation[i]) {
+                deactivation = false;
+            }
+        }
+
+        if (deactivation) {
+            System.out.println("Атаки парні! Механізм деактивовано!");
+        }
+        else {
+            System.out.println("О ні! Монстр атакував вас!");
+        }
+
     }
+
+
 }
